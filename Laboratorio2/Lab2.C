3df7948afd126ee66520830b6b5ff2d0be829cbd
maguer <mariolaguer4@gmail.com>
2019-04-01 22:05:48 -0300

    Parte 1 Lista

--- Laboratorio2/Lab2.C
+++ Laboratorio2/Lab2.C
@@ -3,20 +3,21 @@
 #use LED.LIB
 #use UTILITIES.LIB
 
+#define ON_TIME		400
+#define OFF_TIME	800
+
 main()
 {
-	char i;
-
 	HW_init();
 
 	while(1)
 	{
-
-
-		switch(){
-			balb ladkjdklfjas
+		costate
+		{
+			LED_RED_SET();
+			waitfor(DelayMs(ON_TIME));
+			LED_RED_RESET();
+			waitfor(DelayMs(OFF_TIME));			
 		}
-
-		prueba 1 
 	}
 }
