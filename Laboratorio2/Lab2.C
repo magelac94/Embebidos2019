95198d2fe9a46111609e14976b01de75416fae7b
maguer <mariolaguer4@gmail.com>
2019-04-01 22:28:54 -0300

    Fin de clase 20190401

--- Laboratorio2/Lab2.C
+++ Laboratorio2/Lab2.C
@@ -5,6 +5,7 @@
 
 #define ON_TIME		400
 #define OFF_TIME	800
+#define ONE_SECOND	1000
 
 main()
 {
@@ -12,12 +13,18 @@ main()
 
 	while(1)
 	{
-		costate
+		costate RED_LED always_on
 		{
 			LED_RED_SET();
 			waitfor(DelayMs(ON_TIME));
 			LED_RED_RESET();
 			waitfor(DelayMs(OFF_TIME));			
+		}
+		
+		costate MENU always_on
+		{
+			printf("loop");
+			waitfor(DelayMs(ONE_SECOND));
 		}
 	}
 }
