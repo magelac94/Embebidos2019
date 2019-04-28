efde35193d4353c01241d33b0962ed57bc6b2016
Iñaki Martin <inaki_uy@hotmail.com>
2019-04-28 20:06:14 -0300

    Andan las funcionalidades por consola
    Anda por consola pero no se muestra todo el menu.
    
    Tuve que volver a la consola por que tcp se volvio loco.

--- Laboratorio3_v2/Lab3.C
+++ Laboratorio3_v2/Lab3.C
@@ -1,6 +1,6 @@
 #use IO.LIB
 #use LED.LIB
-#use TCPCON.lib
+//#use TCPCON.lib
 #use EVENTOS.LIB
 #use UTILITIES.LIB
 #use RTC.lib
@@ -11,18 +11,20 @@
 #define ONE_SECOND	1000
 
 struct tm FechaHora;
-char entradaAnalogica[2];
 char opcion_menu[5];
-int int_opcion_menu, int_lugar_libre, int_id_evento;
+int int_opcion_menu, int_lugar_libre, int_id_evento, int_Analog_Value;
 Event unEvento;
 
-cofunc void programaPrincipal( enum tipoUI interfazAUsar ){
+cofunc void programaPrincipal[2]( enum tipoUI interfazAUsar ){
 
-	wfd MENU_mostrarMenuPrincipal( interfazAUsar );
+	MENU_mostrarMenuPrincipal( interfazAUsar );
 	wfd MENU_obtenerOpcion( interfazAUsar, opcion_menu );
 	int_opcion_menu = atoi( opcion_menu );
 
 	switch( int_opcion_menu ){
+		/*case( OPCION_0 ):
+			// MENU PRINCIPAL
+       */
 		case( OPCION_1 ):
 			// FIJAR HORA EN EL RELOJ
 			wfd MENU_pedirFechaHora( &FechaHora, interfazAUsar );
@@ -54,11 +56,12 @@ cofunc void programaPrincipal( enum tipoUI interfazAUsar ){
 			wfd MENU_consultarEventos( interfazAUsar );
 			EVENTOS_listarEventos();
 			break;
-			
+
 		case( OPCION_6 ):
 			// Consulta analogica
-			wfd MENU_pedirEntradaAnalogica( interfazAUsar, &entradaAnalogica );
-			wfd {int_Analog_Value = IO_getAnalogInput( &entradaAnalogica )}; //el valor que toma por parametro es un unsigned char
+			wfd MENU_pedirEntradaAnalogica( interfazAUsar, opcion_menu );
+			int_opcion_menu = atoi(opcion_menu);
+			int_Analog_Value = IO_getAnalogInput( int_opcion_menu ); //el valor que toma por parametro es un unsigned char
 			wfd MENU_mostrarEntradaAnalogica( interfazAUsar, &int_Analog_Value );
 			break;
 
@@ -78,29 +81,21 @@ main(){
 
 // INIT
 	HW_init();
-	/*
-	This function should be called in the beginning of a program that uses single-user cofunctions. It initializes
-	internal data structures that are used by loophead().
-	*/
-	loopinit();
+
 	/* 2.1.4 TCP/IP Stack Initialization
 	The function sock_init() must be called near the start of your main() function in order to initialize
 	the TCP/IP stack. The return value from sock_init() must indicate success before calling any other
 	TCP/IP functions, with the possible exception of ifconfig().
 	*/
-	printf("Iniciando Socket\n");
+/*	printf("Iniciando Socket\n");
 	sock_init();
 	printf("Socket Iniciado\n");
+*/
 	EVENTOS_Eventos_init();
 
 // LOOP MAQUINAS DE ESTADO
 	while(1)
 	{
-		/*
-		This function should be called within the “big loop” in your program.
-		It is necessary for proper single-user cofunction abandonment handling
-		*/
-		loophead();
 		//Maquina de estado para verificar que no estamos colgados
 		costate RED_LED always_on
 		{
@@ -111,22 +106,22 @@ main(){
 		}
 
 		//Maquina de estado para mantener conexion
-		costate CONEXION_TCP always_on
+/*		costate CONEXION_TCP always_on
 		{
 			wfd TCPCON_conexion();
 		}
-
+*/
 		//Maquina de estado para mostrar menu y manipular hora y eventos desde consola
 		costate INSTANCIA_CONSOLA always_on
 		{
-			wfd programaPrincipal( CONSOLA );
+			wfd programaPrincipal[CONSOLA]( CONSOLA );
 		}
 		// Laboratorio 3 - Maquina de estado para manejar los eventos desde TCP.
-		costate INSTANCIA_TCP always_on
+/*		costate INSTANCIA_TCP always_on
 		{
-			wfd programaPrincipal( TCP );
+			wfd programaPrincipal[TCP]( TCP );
 		}
-
+*/
 		//Maquina de estado para checkear si es momento de ejecutar algun evento programado
 		costate EVENT_CHECKER always_on
 		{
