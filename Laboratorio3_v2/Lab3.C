480b98fb93328481cd55c94d874896b057fbaf51
Iñaki Martin <inaki_uy@hotmail.com>
2019-04-28 13:10:48 -0300

    Revert "Agregar instancias"
    This reverts commit c0c730aa3308373e0e075788c93ee6dd3cb94e37.

--- Laboratorio3_v2/Lab3.C
+++ Laboratorio3_v2/Lab3.C
@@ -15,7 +15,7 @@ char opcion_menu[5];
 int int_opcion_menu, int_lugar_libre, int_id_evento;
 Event unEvento;
 
-cofunc void programaPrincipal[2]( enum tipoUI interfazAUsar ){
+cofunc void programaPrincipal( enum tipoUI interfazAUsar ){
 
 	wfd MENU_mostrarMenuPrincipal( interfazAUsar );
 	wfd MENU_obtenerOpcion( interfazAUsar, opcion_menu );
@@ -108,15 +108,15 @@ main(){
 			wfd TCPCON_conexion();
 		}
 
-		// Laboratorio 3 - Maquina de estado para mostrar menu y manipular hora y eventos desde consola
+		//Maquina de estado para mostrar menu y manipular hora y eventos desde consola
 		costate INSTANCIA_CONSOLA always_on
 		{
-			wfd programaPrincipal[CONSOLA]( CONSOLA );
+			wfd programaPrincipal( CONSOLA );
 		}
 		// Laboratorio 3 - Maquina de estado para manejar los eventos desde TCP.
 		costate INSTANCIA_TCP always_on
 		{
-			wfd programaPrincipal[TCP]( TCP );
+			wfd programaPrincipal( TCP );
 		}
 
 		//Maquina de estado para checkear si es momento de ejecutar algun evento programado
