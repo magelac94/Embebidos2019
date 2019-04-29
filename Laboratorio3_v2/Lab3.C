e5d4112d7f93d2b6d1f636ce352c2a1d510d2778
Iñaki Martin <inaki_uy@hotmail.com>
2019-04-28 22:18:37 -0300

    anda por consola
    No muestra el menu completo por algun tema de buffer

--- Laboratorio3_v2/Lab3.C
+++ Laboratorio3_v2/Lab3.C
@@ -1,6 +1,33 @@
+// Integrantes: Aguerrondo, Carballo, Martin
+// Abril 2019 UCU
+/*
+ATENCION: No pudimos utilizar las cofunctions indexadas por que cuando una queda
+esperando y se llama la otra, los estados de la estructura CoData se corrompen.
+Leimos que es una solucion cuando por ejemplo se quiere controlar 25 puertas y hay que
+hacer la misma operacion sobre todas, pero esas operaciones no quedan esperando, comienzan
+y terminan, entonces la estructura CoData asocada se mantiene consistente.
+
+Al usar la consola como salida, el menu principal se ve incompleto como si se llenara el buffer
+pero eso es imposible por que tiene espacio suficiente. No logramos determinar si el error es
+por propiedades de la consola o problemas del codigo. Por TCP se muestra completo.
+
+Si hacemos la salida por TCP, al seleccionar la opcion arranca un bucle infinito donde
+el programa principal siempre sale por la opcion DEFAULT.
+
+Para que solo ande por consola comentamos:
+use TCPCON.lib
+
+printf("Iniciando Socket\n");
+sock_init();
+printf("Socket Iniciado\n");
+
+costate de la instancia TCP
+
+caso TCP en funciones: enviar_a_ui y obtener_de_ui
+*/
 #use IO.LIB
 #use LED.LIB
-//#use TCPCON.lib
+#use TCPCON.lib
 #use EVENTOS.LIB
 #use UTILITIES.LIB
 #use RTC.lib
@@ -15,16 +42,14 @@ char opcion_menu[5];
 int int_opcion_menu, int_lugar_libre, int_id_evento, int_Analog_Value;
 Event unEvento;
 
-cofunc void programaPrincipal[2]( enum tipoUI interfazAUsar ){
+//cofunc void programaPrincipal[2]( enum tipoUI interfazAUsar ){
+cofunc void programaPrincipal( enum tipoUI interfazAUsar ){
 
-	MENU_mostrarMenuPrincipal( interfazAUsar );
+	wfd MENU_mostrarMenuPrincipal( interfazAUsar );
 	wfd MENU_obtenerOpcion( interfazAUsar, opcion_menu );
 	int_opcion_menu = atoi( opcion_menu );
 
 	switch( int_opcion_menu ){
-		/*case( OPCION_0 ):
-			// MENU PRINCIPAL
-       */
 		case( OPCION_1 ):
 			// FIJAR HORA EN EL RELOJ
 			wfd MENU_pedirFechaHora( &FechaHora, interfazAUsar );
@@ -87,15 +112,15 @@ main(){
 	the TCP/IP stack. The return value from sock_init() must indicate success before calling any other
 	TCP/IP functions, with the possible exception of ifconfig().
 	*/
-/*	printf("Iniciando Socket\n");
-	sock_init();
+	printf("Iniciando Socket\n");
 	printf("Socket Iniciado\n");
-*/
+
 	EVENTOS_Eventos_init();
 
 // LOOP MAQUINAS DE ESTADO
 	while(1)
 	{
+		loophead();
 		//Maquina de estado para verificar que no estamos colgados
 		costate RED_LED always_on
 		{
@@ -114,12 +139,14 @@ main(){
 		//Maquina de estado para mostrar menu y manipular hora y eventos desde consola
 		costate INSTANCIA_CONSOLA always_on
 		{
-			wfd programaPrincipal[CONSOLA]( CONSOLA );
+			//wfd programaPrincipal[CONSOLA]( CONSOLA );
+			wfd programaPrincipal( CONSOLA );
 		}
 		// Laboratorio 3 - Maquina de estado para manejar los eventos desde TCP.
 /*		costate INSTANCIA_TCP always_on
 		{
-			wfd programaPrincipal[TCP]( TCP );
+			//wfd programaPrincipal[TCP]( TCP );
+			wfd programaPrincipal( TCP );
 		}
 */
 		//Maquina de estado para checkear si es momento de ejecutar algun evento programado
