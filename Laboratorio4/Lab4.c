92e0c996bd131ea5948f3e3ce0139607c7c68095
magelac94 <magelac94@gmail.com>
2019-04-29 22:34:27 -0300

    task led
    task led

--- Laboratorio4/Lab4.c
+++ Laboratorio4/Lab4.c
@@ -1,6 +1,23 @@
 #use IO.LIB
 #use ucos2.LIB
+#use LED.LIB
+
+void Red_led(){
+while(1){
+	LED_RED_SET();
+	OSTimeDlySec(1);
+	LED_RED_RESET();
+	OSTimeDlySec(1);
+   }
+}
+
 
 main(){
 	HW_init();
+	OSInit();
+
+	OSTaskCreate(Red_led, NULL, 256, 4);
+
+
+	OSStart();
 }
