1a293106260a3786789d795c6e070733c0ef63e9
magelac94 <magelac94@gmail.com>
2019-05-10 12:18:23 -0300

    Consola
    Anda la consola sin el tcp
    El tcp imprime pero no obtiene valores mas de un caracter.

--- Laboratorio4/Lab4v2.c
+++ Laboratorio4/Lab4v2.c
@@ -1,18 +1,14 @@
 //#class auto 			// Change default storage class for local variables: on the stack
 #define DEBUG			// activo para imprimir estado de conexion TCP
-#define NOANDA        //Activo si quiero probar el caso que no anda
+//#define NOANDA        //Activo si quiero probar el caso que no anda
 
 /* uCOS configuration */
 #define OS_MAX_TASKS			2  		// Maximum number of tasks system can create (less stat and idle tasks)
 #define OS_TASK_SUSPEND_EN		1		// Habilitar suspender y resumir tareas
 #define OS_TIME_DLY_HMSM_EN		1		// Habilitar la funcion de delay para pasar fecha y hora
 #define STACK_CNT_256			2		// Led_Red + idle
-#define STACK_CNT_512			1		// main()
+#define STACK_CNT_512			2		// main()
 #define STACK_CNT_2K			1		// 1 Tarea TCP
-
-/* Definimos la prioridad de las tareas */
-#define TASK3			3
-#define TASK4			4
 
 /* TCP/IP configuration */
 #define TCPCONFIG 0
@@ -22,11 +18,13 @@
 #define PORT 7
 #define MY_GATEWAY "10.10.0.1"
 #define TAMANIO_BUFFER_LE 512        // Este es el tamanio que le damos a nuestros buffers para leer y enviar al socket
-
+//#define TCP_MODE_ASCII 1
 /* Incluimos las librerias luego de los define para sobre escribir los macros deseados */
 #use IO.LIB
 #use LED.LIB
 #use EVENTOS.LIB
+#use UTILITIES.LIB
+#use RTC.lib
 
 
 #memmap xmem
@@ -50,7 +48,84 @@ void Led_Red(void* pdata){
 }
 
 // Tarea 2 - Menu por consola
-void interfaz_consola(void* pdata);
+void interfaz_consola(void* pdata){
+	Event unEvento;
+	struct tm FechaHora;
+	char opcion_menu;
+	char aux[2];
+	char buffer[50];
+	int int_opcion_menu, int_id_evento, int_Analog_Value;
+    enum tipoUI interfazAUsar;
+    interfazAUsar = *(int*)pdata;
+    printf("INTERFAZ A USAR POR CONSOLA LEEE %d\n",interfazAUsar );
+
+//    // Defino Array de Eventos
+//    Event eventos[MAX_EVENTS];		// Lista de Eventos
+//    EVENTOS_Eventos_init(&eventos);   // Inicializo array de eventos para cada llamada a programa principal, son n instancias, son n arrays;
+
+    while(1){ // LOOP menu
+
+    	// Muestra Menu y pide una opcion
+		MENU_mostrarMenuPrincipal( CONSOLA, buffer, NULL );
+		MENU_obtenerOpcion( CONSOLA, &opcion_menu, NULL );
+		int_opcion_menu = atoi( &opcion_menu );
+
+		// Evaluacion de la Opcion
+		switch( int_opcion_menu ){
+			case( OPCION_1 ):
+				// CONFIGURAR FECHA HORA
+				MENU_pedirFechaHora( &FechaHora, CONSOLA, NULL, buffer );
+				RTC_fijarFechaHora( &FechaHora );
+				MENU_printFechaHora( &FechaHora, CONSOLA );		// Imprimo la Fecha y hora modificadoS
+
+				break;
+
+			case( OPCION_2 ):
+				// CONSULTAR FECHA HORA ACTUAL
+				MENU_consultarHora( CONSOLA, NULL );
+				RTC_leerFechaHora( &FechaHora );	// Leo el RTC
+				MENU_printFechaHora( &FechaHora, CONSOLA ); // Imprimo la Fecha y hora
+				break;
+
+			case( OPCION_3 ):
+				// AGREGAR EVENTO
+				MENU_pedirDatosEvento( &unEvento, &FechaHora, CONSOLA, NULL, buffer );
+			//	EVENTOS_agregarEvento( &eventos, &unEvento ); // ver el tema del array de eventos, lo agregue en el mail lo saque del Eventos para poder manejar diferentes array , 1 por instancia, a no ser que sea todo el emis
+				break;
+			case ( OPCION_4 ):
+				// ELIMINAR EVENTO SEGUN EL NUMERO DE EVENTO (ES DE 1 EN adelante segun posicion en el array))
+				int_id_evento = MENU_eliminarEvento( CONSOLA, NULL, buffer );
+				printf("%d\n", int_id_evento);
+			//	EVENTOS_eliminarEvento( int_id_evento );  // revisar despues de agregarEvento
+				break;
+			case( OPCION_5 ):
+				// CONSULTAR EVENTO
+				MENU_consultarEventos( CONSOLA , NULL);
+				EVENTOS_listarEventos();
+				break;
+
+			case( OPCION_6 ):
+				// CONSULTA ANALOGICA
+				MENU_pedirEntradaAnalogica( CONSOLA, aux, NULL, buffer );
+				int_opcion_menu = atoi(aux);
+				int_Analog_Value = IO_getAnalogInput( int_opcion_menu ); //el valor que toma por parametro es un unsigned char
+				MENU_mostrarEntradaAnalogica( CONSOLA, &int_Analog_Value, NULL );
+				break;
+			case( OPCION_7 ):
+				// SALIR
+				printf("Salir");
+				break;
+
+
+			default:
+				// OPCION INCORRECTA
+				printf("\nOpcion DEFAULT: %d \n", int_opcion_menu);
+				printf("Vuelva a ingresar\n");
+			}
+
+		//OSTaskSuspend(OS_PRIO_SELF);
+		}
+}
 
 // Tarea 3 - Menu por TCP
 /* Esta tarea escucha por una conexion entrante por el puerto 7.
@@ -58,10 +133,10 @@ Cuando hay una conexion activa, muestra el menu por la interfaz tcp.
 Debemos asignarle un stack de 2K por el socket tcp.
 */
 void interfaz_tcp(void* pdata){
-
-	static char buffer_recepcion[TAMANIO_BUFFER_LE], buffer_envio[TAMANIO_BUFFER_LE];
+	struct tm FechaHora;
+	char buffer_recepcion[TAMANIO_BUFFER_LE], buffer_envio[TAMANIO_BUFFER_LE];
 	static tcp_Socket un_tcp_socket;
-	auto int bytes_leidos, bytes_enviados, i;
+	auto int bytes_leidos, bytes_enviados, i, int_opcion_menu;
 
 	while(1) {
 		// Ponemos el socket en estado de escucha
@@ -90,26 +165,34 @@ void interfaz_tcp(void* pdata){
 		printf("\nDEBUG: Conexion establecida\n");
 #endif
 
-		//Hago primero y al final actualizo el socket
-		// Mandamos y revibimos del socket
 		do {
-			// Aca pongo el programa principal. Cuando espero por input hago ticks mientras espero
-			// Esto es una prueba de concepto.
+
+			// Se muestra menu y se espera opcion
 			MENU_mostrarMenuPrincipal( TCP, buffer_envio, &un_tcp_socket );
-
-#ifndef NOANDA
-         bytes_enviados=sock_fastwrite( &un_tcp_socket, buffer_envio, sizeof(buffer_envio)-1 );
-			if(bytes_enviados>0) {
-	         for ( i = 0; i < bytes_enviados; i++ ){
-	            buffer_envio[i] = ' ';
-	         }
-			}
-#endif
-
 			MENU_obtenerOpcion( TCP, buffer_recepcion, &un_tcp_socket );
-#ifdef DEBUG
-         printf("\nDEBUG: Hemos recibido: %s", buffer_recepcion );
-#endif
+
+
+         	int_opcion_menu = atoi( buffer_recepcion );
+         	printf("%u\n", int_opcion_menu );
+
+         	switch( int_opcion_menu ){
+				case( OPCION_1 ):
+					// CONFIGURAR FECHA HORA
+					printf("\nOPCION 1\n");
+					MENU_pedirFechaHora( &FechaHora, TCP, &un_tcp_socket, buffer_recepcion );
+					//RTC_fijarFechaHora( &FechaHora );
+					break;
+				default:
+					// OPCION INCORRECTA
+					printf("\nOpcion DEFAULT: %d \n", int_opcion_menu);
+					printf("Vuelva a ingresar\n");
+			}
+
+
+
+
+
+
 
 		} while(tcp_tick(&un_tcp_socket));
 #ifdef DEBUG
@@ -146,8 +229,9 @@ main(){
 	OSSchedLock();
 
 	//Creacion de tareas
-	Error = OSTaskCreate(Led_Red, NULL, 256, TASK3 );
-	Error = OSTaskCreate(interfaz_tcp, NULL, 2048, TASK4 );
+	Error = OSTaskCreate(Led_Red, NULL, 256, 3 );
+	//	Error = OSTaskCreate(interfaz_tcp, NULL, 2048, 4 );
+	Error = OSTaskCreate(interfaz_consola,NULL,512, 5);
 
 	// Re-habilitamos scheduling
 	OSSchedUnlock();
