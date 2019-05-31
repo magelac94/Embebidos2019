fcd57289594fe81b684944e9afbcb51cbf108d94
magelac94 <magelac94@gmail.com>
2019-05-31 19:29:44 -0300

    Update Lab5GSM.c
    Se borra contenido de variable dataRx

--- Laboratorio5GSM/Lab5GSM.c
+++ Laboratorio5GSM/Lab5GSM.c
@@ -10,13 +10,13 @@
 
 
 void main(){
-	
+
 	int status;
 	char dataRx[GPRS_MSG_LENGTH];
-	
+
 	// Configuramos STATUS asi despues podemos leer el pin
-	BitWrPortI(PEDDR, &PEDDRShadow, INPUT_DIR, BIT_1); 
-	
+	BitWrPortI(PEDDR, &PEDDRShadow, INPUT_DIR, BIT_1);
+
 	// Configuramos puerto serial D
 	serDopen(GPRS_BAUD_RATE);
 	serDrdFlush();
@@ -36,29 +36,31 @@ void main(){
 		UT_delay(2000);
 		status = IO_getInput(PORT_E, BIT_1);
 	}
-	
+
 	serDputs("A");
 	UT_delay(6000);
 	serDputs("AT\r");
 	UT_delay(1000);
-	
+
 	do {
-		UT_delay(100)
+		UT_delay(100);
 	}while(!serDrdUsed());
 
+   memset(dataRx, 0, sizeof(dataRx));
    serDread(dataRx, sizeof(dataRx), TIMEOUT_10);
    printf("Respuesta del modem: %s", dataRx);
    //printf(dataRx); si sale basura del printf de arriba, ver con este a ver si es el tipo de dato %s el problema
-   
-   /* 
+
+   /*
 	Dicen que tenemos que chequear siempre antes de operar que el modem no este muerto.
 	No se si aca antes de intentar ver si estamos registrados, en este caso de prueba vale la pena.
 	*/
 	serDputs("AT+CPIN?\r");
 	UT_delay(1000);
+   memset(dataRx, 0, sizeof(dataRx));
 	serDread(dataRx, sizeof(dataRx), TIMEOUT_10);
 	printf("Respuesta del modem: %s", dataRx);
-	
+
 }
 
 
@@ -67,9 +69,9 @@ void main(){
 	O podriamos tener una funcion que se llame checkStatus que mire si esta vivo, y
 	si esta apagado, llama a esta de modemON
 	*/
-int modemON(){	
+int modemON(){
 	int status;
-	
+
 	status = IO_getInput(PORT_E, BIT_1);
 	while(!status){
 		BitWrPortI(PEDDR, &PEDDRShadow, OUTPUT_DIR, BIT_4);
