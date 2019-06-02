ee0fcfe185d7caa3be8e4653bb4c3eee5e493e86
magelac94 <magelac94@gmail.com>
2019-06-02 17:23:57 -0300

    Update pruebaGPS.c
    Imprimir posicion. imprime 4864 2697. parece no estar bien. en grados deberia ser 34,56

--- Laboratorio5GPS/pruebaGPS.c
+++ Laboratorio5GPS/pruebaGPS.c
@@ -10,22 +10,51 @@
 
 #use RTC.lib
 #use GPS_Custom.lib
-//#use IO.LIB
+#use LED.LIB
+#use IO.LIB
 #use ucos2.lib
+//#use MENU.LIB
+
+void tarea_led_red(void* pdata){
+
+	while(1){
+		LED_RED_SET();
+		OSTimeDlySec(1);
+		LED_RED_RESET();
+		OSTimeDlySec(1);
+	}
+}
+void imprimirPoscion(GPSPosition *pos);
+void imprimirPoscion(GPSPosition *pos){
+
+	int latgrados,longrados;
+
+	latgrados = pos->lat_degrees;
+	longrados = pos->lon_degrees;
+	
+	printf("%d\n", latgrados );
+	printf("%d\n", longrados );
+	
+}
 
 void GPS_configRTC(void* pdata){
 	struct tm FechaHora;
-	struct tm p_dateTime;
+	struct tm* p_dateTime;
 	unsigned long timestampGPS;
 	unsigned long timestampRabbit;
+   GPSPosition *posicion;
 
 	char* p_str;
 	char buffer[500];
 
-//	while(true){
-		// Obtengo Fecha del GPS
-		gps_get_utc( &p_dateTime, p_str);
+	while(1){
+   	// Imprimo Trama completa
+      printf("%s \n", pdata);
 
+   	// Obtengo Fecha del GPS
+		gps_get_utc( p_dateTime, pdata);
+
+      // Imprimo Fecha
  //		printFechaHora(0,p_dateTime,buffer, NULL)
 
 	/*	timestampGPS = mktime( &p_dateTime );
@@ -35,8 +64,13 @@ void GPS_configRTC(void* pdata){
 
 		// Evaluo si la fecha obtenida es igual a la que ya esta configurada
 		if (timestampGPS != timestampRabbit){
-			RTC_fijarFechaHora( &p_dateTime );
-		}                             */
+			RTC_fijarFechaHora( &p_dateTime ); */
+
+		gps_get_position(posicion, pdata);
+
+		imprimirPoscion(posicion);
+
+		}
 }
 
 
@@ -48,15 +82,20 @@ main() {
 
 	char* p_str;
 
-	//HW_init();
+	HW_init();
 
 	OSInit();
 
-	OSTaskCreate(GPS_init, NULL, 256, 2);  // Inicializa Hardware GPS - Ejecuta 1 vez
-	OSTaskCreate(GPS_gets, p_str, 256, 3); // Se obtiene datos gps
+	OSTaskCreate(tarea_led_red, NULL, 256, 2);
+	OSTaskCreate(GPS_init, NULL, 256, 3);  // Inicializa Hardware GPS - Ejecuta 1 vez
+	OSTaskCreate(GPS_gets, p_str, 256, 4); // Se obtiene datos gps
 
-	printf("%s", p_str); // imprimo trama
-  //	OSTaskCreate(GPS_configRTC, NULL, 256, 11); // Configura el Reloj Periodicamente
+ //	printf("%s", p_str); // imprimo trama
+  	OSTaskCreate(GPS_configRTC, p_str, 256, 11); // Configura el Reloj Periodicamente
+
+  //	gps_get_utc( &p_dateTime, p_str);
+
+
 	OSStart();
 
 }
