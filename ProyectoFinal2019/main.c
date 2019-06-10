ef9eece7a8b9b3ea8eae855513f1b1a1dad6fee0
Iñaki Martin <inaki_uy@hotmail.com>
2019-06-09 22:34:47 -0300

    GPRS, TCP y CONSOLA
    La consola para poder mandar comandos directo al modem para ir probando las funciones. Es mas que nada una herramienta para nosotros.

--- ProyectoFinal2019/main.c
+++ ProyectoFinal2019/main.c
@@ -1,13 +1,13 @@
 #define DEBUG			// activo para imprimir mensajes de DEBUG
 
 /* uCOS configuration */
-#define OS_MAX_TASKS			1  		// Cantidad maxima de tareas que se pueden crear, sin contar STAT e IDLE
+#define OS_MAX_TASKS			3  		// Cantidad maxima de tareas que se pueden crear, sin contar STAT e IDLE
 #define OS_TASK_SUSPEND_EN		1		// Habilitar suspender y resumir tareas
 #define OS_TIME_DLY_HMSM_EN		1		// Habilitar la funcion de delay para pasar fecha y hora
 #define OS_SEM_EN				1		// Habilitar semaforos
 #define OS_MAX_EVENTS			1		// MAX_TCP_SOCKET_BUFFERS + 0 Mbox + 0 Queue + 0 Semaforos
 #define STACK_CNT_256			2		// tarea_Led_Red + idle
-#define STACK_CNT_512			1		// main()
+#define STACK_CNT_512			3		// main() + GPRS_tarea_encender_modem + CONSOLA_tarea_comandos_a_mano
 #define STACK_CNT_2K			1		// 1 Tareas TCP (MAX_TCP_SOCKET_BUFFERS)
 
 /* TCP/IP configuration */
@@ -22,15 +22,18 @@
 #define TAMANIO_BUFFER_LE 		512      			// Este es el tamanio que le damos a nuestros buffers para leer y enviar al socket
 
 /* Incluimos las librerias luego de los define para sobre escribir los macros deseados */
-#use IO.LIB
-#use LED.LIB
+#use IO.lib
+#use LED.lib
+#use GPRS.lib
+#use CONSOLA.lib
+#use Utilities.lib
 
 #memmap xmem
 #use "ucos2.lib"
 #use "dcrtcp.lib"
 
 main(){
-	
+
 	// Variables
 	auto INT8U Error;
 	static tcp_Socket un_tcp_socket[MAX_TCP_SOCKET_BUFFERS];
@@ -40,7 +43,7 @@ main(){
 
 	// Inicializa la estructura de datos interna del sistema operativo uC/OS-II
 	OSInit();
-	
+
 	// Iniciamos el stack TCP/IP
 #ifdef DEBUG
 	printf("\nDEBUG: Iniciando Sockets\n");
@@ -54,15 +57,16 @@ main(){
 	OSSchedLock();
 
 	//Creacion de tareas
-	Error = OSTaskCreate(tarea_led_red, NULL, 256, 5);
-//	Error = OSTaskCreate(tarea_modem, NULL, OJO, 6);	//IÑAKI
-//	Error = OSTaskCreate(tarea_gps, NULL, OJO, 7 );		// MAGELA
-//	Error = OSTaskCreate(tarea_interfaz_tcp, &un_tcp_socket[0], 2048, 8 ); //IÑAKI
-//	Error = OSTaskCreate(tarea_botones,NULL, OJO, 9);	// MARIO
-//	Error = OSTaskCreate(tarea_salud,NULL, OJO, 10);	//MARIO
-// 	Error = OSTaskCreate(tarea_config_Reloj,NULL, OJO, 11)  // MAGELA
-	
-	// Re-habilitamos scheduling
+	Error = OSTaskCreate(LED_tarea_led_red, NULL, 256, 5);
+	Error = OSTaskCreate(GPRS_tarea_encender_modem, NULL, 512, 6);	//INAKI
+	Error = OSTaskCreate(CONSOLA_tarea_comandos_a_mano, NULL, 512,7);
+//	Error = OSTaskCreate(tarea_gps, NULL, OJO, 8 );		// MAGELA
+//	Error = OSTaskCreate(TCP1_tarea_interfaz_tcp, &un_tcp_socket[0], 2048, 9 ); //INAKI
+//	Error = OSTaskCreate(tarea_botones,NULL, OJO, 10);	// MARIO
+//	Error = OSTaskCreate(tarea_salud,NULL, OJO, 11);	//MARIO
+// 	Error = OSTaskCreate(tarea_config_Reloj,NULL, OJO, 12)  // MAGELA
+
+// Re-habilitamos scheduling
 	OSSchedUnlock();
 
 	// Iniciamos el sistema operativo comenzando por la tarea en estado "ready" de mayor prioridad
